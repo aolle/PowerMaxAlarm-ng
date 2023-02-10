9bebcc40e575db6fb54710e2d706c3ffa8eccb22
Àngel Ollé Blázquez <angel@olleb.com>
2023-02-10 02:11:56 +0100

    Add config

---
+++ include/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Wifi Secrets
+#define WIFI_SSID "XXX"
+#define WIFI_PASS "XXXXXX"
+
+// WifiClient Configuration
+#define IP \
+    { 192, 168, 1, 22 }
+#define GATEWAY \
+    { 192, 168, 1, 1 }
+#define MASK \
+    { 255, 255, 255, 0 }
+#define PRIMARY_DNS \
+    { 8, 8, 8, 8 }
+#define SECONDARY_DNS \
+    { 8, 8, 4, 4 }
+
+// OTA
+#define OTA_PASSWORD "xxxxxx"
